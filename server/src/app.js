74e9d97eb04aa77a3a81e018ce5c5c5c5a788438
melkie yilk <melkieyilk@gmail.com>
2025-03-07 20:46:17 +0300

    import and add the launches router

--- server/src/app.js
+++ server/src/app.js
@@ -1,8 +1,10 @@
+const path = require("path");
 const express = require("express");
 const cors = require("cors");
-const path = require("path");
 const morgan = require("morgan");
+
 const planetsRouter = require("./routes/planets/planets.router");
+const launchesRouter = require("./routes/lauches/launches.router");
 
 const app = express();
 
@@ -13,14 +15,18 @@ app.use(cors()); //allow all to acess all sites
 //     origin: "http://localhost:8000",
 //   })
 // ); // for specific origin
+
 app.use(morgan("combined"));
 
 app.use(express.json());
 
-// app.use(express.static(path.join(__dirname, "..", "public")));
-// app.use("/", (req, res) => {
-//   res.sendFile(path.join(__dirname, "..", "public", "index.html"));
-// });
+app.use(express.static(path.join(__dirname, "..", "public")));
+
 app.use(planetsRouter);
+app.use(launchesRouter);
+
+app.use("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "public", "index.html"));
+});
 
 module.exports = app;
